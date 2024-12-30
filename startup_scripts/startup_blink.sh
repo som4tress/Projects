6600ecf8bd9d9c8ee16bef02ad86eb8a3c60c63f
Somdutta Roy <som4tress@gmail.com>
2024-12-30 13:39:28 -0800

    Initialize through the venv

--- startup_scripts/startup_blink.sh
+++ startup_scripts/startup_blink.sh
@@ -7,5 +7,6 @@ echo "Waiting for 30 sseconds before launching Blink Automation"
 sleep 30
 
 #source /home/pluto/blinkpy/blinkpy/bin/activate
+source /home/pluto/Projects/.venv/bin/activate
 cd /home/pluto/Projects/BlinkAutomation/
 while true; do echo "Launching Blink Automation"; /home/pluto/blinkpy/blinkpy/bin/python3 BlinkAutomation.py; sleep 30; done
